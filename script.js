d8cfe0f9cef0362f979bf95b6ee7abae1b02f3f3
abdel <abdelnour.rekkab@gmail.com>
2024-03-11 01:12:49 +0100

    Game's Physics/Movement/Obstacles placement

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+//----------------
 //Game Board
+//----------------
 let gameBoard;
 let gameBoardWidth = 360;
 let gameBoardHeight = 640;
 let context;
 
-//character we playing
+//----------------
+//Character
+//----------------
 let charWidth = 34;
 let charHeight = 24;
 let charX = gameBoardWidth/8;
@@ -18,11 +22,23 @@ let char = {
     height : charHeight,
 }
 
+//----------------
+//Obstacles
+//----------------
 let obstacle= [];
 let obstacleWidth = 64;
 let obstacleHeight = 512;
 let obstacleX = gameBoardWidth;
 let obstacleY = 0;
+let topObstacleImg;
+let bottomObstacleImg;
+
+//----------------
+//Physics
+//----------------
+let movementGravity = 0.4; 
+let movementX = -2; //Obstacle speed movement
+let movementY = 0; 
 
 
 window.onload = function() {
@@ -39,7 +55,14 @@ window.onload = function() {
         context.drawImage(charCanvas,char.x,char.y,char.width,char.height);
     }
 
+    topObstacleImg = new Image();
+    topObstacleImg.src = "assets/toppipe.png";
+    bottomObstacleImg = new Image();
+    bottomObstacleImg.src = "assets/bottompipe.png";
+
     requestAnimationFrame(update);
+    setInterval(placeObstacle, 1500);
+    document.addEventListener("keydown", moveChar)
 }
 
 //Game loop
@@ -48,6 +71,48 @@ function update(){
     context.clearRect(0,0,gameBoard.width,gameBoard.height);
 
     //char
+    movementY += movementGravity;
+    char.y += movementY;
     context.drawImage(charCanvas,char.x,char.y,char.width,char.height);
 
+    //Obstacle
+    for (let i = 0; i< obstacle.length; i++){
+        let obs = obstacle[i];
+        obs.x += movementX;
+        context.drawImage(obs.img,obs.x,obs.y,obs.width,obs.height);
+    }
+
+}
+
+function placeObstacle(){
+    console.log("placeObstacle")
+    passThrough = gameBoard.height/4;
+    let randomObsY = obstacleY - obstacleHeight/4 - Math.random()*(obstacleHeight/2);
+    let topObstacle ={
+        img : topObstacleImg,
+        x : obstacleX,
+        y : randomObsY,
+        width : obstacleWidth,
+        height : obstacleHeight,
+        checked : false
+    }
+    
+    obstacle.push(topObstacle);
+    let bottomObstacle={
+        img : bottomObstacleImg,
+        x : obstacleX,
+        y : randomObsY + obstacleHeight + passThrough,
+        width : obstacleWidth,
+        height : obstacleHeight,
+        checked : false
+
+    }
+    obstacle.push(bottomObstacle);
+
+}
+
+function moveChar(e){
+    if (e.code == "Space" || e.code == "ArrowUp"){
+        movementY = -6;
+    }
 }
